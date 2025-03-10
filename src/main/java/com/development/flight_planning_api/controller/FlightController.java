a0f3637306b6ef715fd0a27d930eec825e920f7a
deimar <deimarparn14@gmail.com>
2025-03-10 21:27:36 +0200

    Created flight controller

---
+++ src/main/java/com/development/flight_planning_api/controller/FlightController.java
@@ -0,0 +1,24 @@
+package com.development.flight_planning_api.controller;
+
+import com.development.flight_planning_api.model.Flight;
+import com.development.flight_planning_api.service.FlightService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/flights")
+public class FlightController {
+    private final FlightService flightService;
+
+    @Autowired
+    public FlightController(FlightService flightService) {
+        this.flightService = flightService;
+    }
+
+    @GetMapping
+    public List<Flight> getAllFlights() {
+        return flightService.getAllFlights();
+    }
+}
