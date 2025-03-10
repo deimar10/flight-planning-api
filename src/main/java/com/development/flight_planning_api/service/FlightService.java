afeadbf8b7411f414466d6a763255733158eafed
deimar <deimarparn14@gmail.com>
2025-03-10 21:25:47 +0200

    Created service layer

---
+++ src/main/java/com/development/flight_planning_api/service/FlightService.java
@@ -0,0 +1,21 @@
+package com.development.flight_planning_api.service;
+
+import com.development.flight_planning_api.model.Flight;
+import com.development.flight_planning_api.repository.FlightRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class FlightService {
+    private final FlightRepository flightRepository;
+
+    @Autowired
+    public FlightService(FlightRepository flightRepository) {
+        this.flightRepository = flightRepository;
+    }
+
+    public List<Flight> getAllFlights() {
+        return flightRepository.findAll();
+    }
+}
