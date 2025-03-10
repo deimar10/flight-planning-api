dfccfafa88e0c1f4fabed7cdff431f9594873f19
deimar <deimarparn14@gmail.com>
2025-03-10 19:42:27 +0200

    Initial commit

---
+++ src/main/java/com/development/flight_planning_api/FlightPlanningApiApplication.java
@@ -0,0 +1,13 @@
+package com.development.flight_planning_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightPlanningApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightPlanningApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/development/flight_planning_api/FlightPlanningApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.development.flight_planning_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightPlanningApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
