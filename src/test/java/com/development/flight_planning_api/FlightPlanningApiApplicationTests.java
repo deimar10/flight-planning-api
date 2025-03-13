52339c936abc4016dadd2351f73a7d28661564ba
deimar <deimarparn14@gmail.com>
2025-03-14 01:56:03 +0200

    Disabled test class

--- src/test/java/com/development/flight_planning_api/FlightPlanningApiApplicationTests.java
+++ src/test/java/com/development/flight_planning_api/FlightPlanningApiApplicationTests.java
@@ -1,9 +1,11 @@
 package com.development.flight_planning_api;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled  // This disables the test class
 class FlightPlanningApiApplicationTests {
 
 	@Test
